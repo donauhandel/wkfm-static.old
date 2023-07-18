f9ea7f9d6e31198b7071c1b5d00cf7fd6b0de98e
csae8092 <peter.andorfer@oeaw.ac.at>
2023-07-18 12:58:20 +0200

    added content to fetch_data script

---
+++ fetch_data.sh
@@ -0,0 +1,12 @@
+# bin/bash
+
+echo "fetching transkriptions from wkfm-working-data"
+rm -rf data/editions 
+rm -rf data/meta 
+curl -LO https://github.com/donauhandel/wkfm-working-data/archive/refs/heads/main.zip
+unzip main
+mv ./wkfm-working-data-main/data/editions/ ./data
+mv ./wkfm-working-data-main/data/meta/ ./data
+
+rm main.zip
+rm -rf ./wkfm-working-data-main
