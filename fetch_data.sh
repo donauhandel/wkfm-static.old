167d85094c3b8e30bc579950b1abac191349dfe3
csae8092 <peter.andorfer@oeaw.ac.at>
2023-07-21 12:34:12 +0200

    added listperson

--- fetch_data.sh
+++ fetch_data.sh
@@ -3,12 +3,18 @@
 echo "fetching transkriptions from wkfm-working-data"
 rm -rf data/editions 
 rm -rf data/meta 
+rm -rf data/indices
 curl -LO https://github.com/donauhandel/wkfm-working-data/archive/refs/heads/main.zip
 unzip main
 mv ./wkfm-working-data-main/data/editions/ ./data
 mv ./wkfm-working-data-main/data/meta/ ./data
-
 rm main.zip
 rm -rf ./wkfm-working-data-main
+
+curl -LO https://github.com/donauhandel/krems-data/archive/refs/heads/main.zip
+unzip main
+mv ./krems-data-main/data/indices/ ./data
+rm main.zip
+rm -rf ./krems-data-main
 
 add-attributes -g "data/editions/*.xml" -b "https://id.acdh.oeaw.ac.at/wkfm"
